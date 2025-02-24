44881c384e33f0a30e9e58ea88b1da79bff6f452
Dong Woo Lee <59324487+don04lee@users.noreply.github.com>
2025-02-24 17:06:29 -0600

    Add model and serializer for country and fact
    Helps with providing frame and converting Django to JSON when API call is made

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse
+from rest_framework import status
+from .models import Fact, Country
+from .serializer import FactSerializer
 
 
 @api_view(["GET"])
 def get_fact(request):
-    return
+    serializer
+    return Response()
