5d842575935a84b1afb2c7e31e22e32dedbc7144
junieboi <jhong95@wisc.edu>
2025-04-16 22:11:09 -0500

    Removed TestAtmosphere.jsx from testcomponents folder. Not needed anymore. For decluttering folders.

--- frontend/statmap-app/src/components/Landing.jsx
+++ frontend/statmap-app/src/components/Landing.jsx
@@ -283,6 +283,13 @@ const Landing = () => {
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
         {modalContent}
       </Modal>
+
+      <button
+        className="fixed bottom-1 right-1 text-opacity-0 text-transparent py-2 px-4 mt-40 rounded-lg hover:text-blue-500"
+        onClick={() => navigate("/globeModeTest")}
+      >
+        GlobeModeTest
+      </button>
     </div>
   );
 };
