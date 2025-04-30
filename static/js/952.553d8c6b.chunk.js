54a3f01b43429e38c841046c9734d814eb28e396
hogantyler <hogantyler03@gmail.com>
2025-04-30 14:53:49 -0500

    Updates

--- static/js/952.553d8c6b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkstatmap_app=self.webpackChunkstatmap_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.553d8c6b.chunk.js.map
